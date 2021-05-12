611bca91fdb2a7cbe53339a6015ef98c08862adf
Thang Duong Bao <thangdb@solarbk.vn>
2021-05-12 15:51:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -f "/root/init-script/service_init.py" ]; then
-    /usr/bin/python3 /root/init-script/service_init.py > /root/init-script/service_init_out.txt
+    /usr/bin/python3 /root/init-script/service_init.py > /root/service_init_out.txt
 else
-	echo "File /root/init-script/service_init.py is not found!" > /root/init-script/service_init_out.txt
+	echo "File /root/init-script/service_init.py is not found!" > /root/service_init_out.txt
 fi
 
 tail -f /dev/null
