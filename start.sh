c7aa8d4035ecb73e6dc2b0fc8fbe77e8dc56da93
Thang Duong Bao <thangdb@solarbk.vn>
2021-05-12 15:37:11 +0700

    Fix init script path when mount using yaml file

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ -f "/root/service_init.py" ]; then
-    /usr/bin/python3 /root/service_init.py > service_init_out.txt
+if [ -f "/root/init-script/service_init.py" ]; then
+    /usr/bin/python3 /root/init-script/service_init.py > /root/init-script/service_init_out.txt
 else
-	echo "File /root/service_init.py is not found!" > service_init_out.txt
+	echo "File /root/init-script/service_init.py is not found!" > /root/init-script/service_init_out.txt
 fi
 
 tail -f /dev/null
